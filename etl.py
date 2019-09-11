3c3e76b626a16fbb93228a25b02b73f40ff3966e
Leandro Queiroz <lerqueiroz@gmail.com>
2019-09-11 12:03:45 -0300

    Add configparser and sparksession code
    feat: Added configparser and sparksession code. Added dl.cfg config file.

--- etl.py
+++ etl.py
@@ -1,6 +1,18 @@
 from pyspark.sql import SparkSession
+import configparser
+import os
+
+config = configparser.ConfigParser()
+config.read('dl.cfg')
+
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
 
 def create_spark_session():
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .getOrCreate()
     return spark
 
 def process_song_data(spark, input_data, output_data):
