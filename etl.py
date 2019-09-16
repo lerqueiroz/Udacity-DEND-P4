16baa7a156d73e0d5b0f858a8328c402a443afc3
Leandro Queiroz <lerqueiroz@gmail.com>
2019-09-15 21:47:27 -0300

    Add schema to log dataset
    feat: Defined schema to log dataset

--- etl.py
+++ etl.py
@@ -39,6 +39,34 @@ def process_song_data(spark, input_data, output_data):
 
 def process_log_data(spark, input_data, output_data):
 
+    # get filepath to log data file
+    log_data = input_data.join("log_data/2018/11/*")
+
+    #defined schema
+    schema_log = StructType([
+    StructField("artist", StringType(), True),
+    StructField("auth", StringType(), True),
+    StructField("firstName", StringType(), True),
+    StructField("gender", StringType(), True),
+    StructField("itemInSession", LongType(), True),
+    StructField("lastName", StringType(), True),
+    StructField("length", DoubleType(), True),
+    StructField("level", StringType(), True),
+    StructField("location", StringType(), True),
+    StructField("method", StringType(), True),
+    StructField("page", StringType(), True),
+    StructField("registration", DoubleType(), True),
+    StructField("sessionId", LongType(), True),
+    StructField("song", StringType(), True),
+    StructField("status", LongType(), True),
+    StructField("ts", LongType(), True),
+    StructField("userAgent", StringType(), True),
+    StructField("userId", StringType(), True)
+    ])
+    
+    # read log data file
+    df_log = spark.read.json(log_data, schema=schema_log)
+	
 def main():
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
